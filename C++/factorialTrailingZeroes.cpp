56f36b021b4e677c72b6bafa7c226aa7162d6515
Qu-Detao <qudet91@gmail.com>
2016-02-22 16:07:04 +0800

    Add solution for "Factorial Trailing Zeroes"

---
+++ C++/factorialTrailingZeroes.cpp
@@ -0,0 +1,17 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+*/
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int power = int(log10(n) / log10(5));
+        int num = 0;
+        for(int i = 1; i <= power; ++i){
+            num += n / pow(5, i);
+        }
+        return num;
+    }
+};
