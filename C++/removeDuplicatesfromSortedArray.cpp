5a06c9bf5321a46b0bb721cd8736014dfb99fb29
Qu-Detao <qudet91@gmail.com>
2016-02-22 13:42:06 +0800

    Add new solution for "Remove Duplicates from Sorted Array"

--- C++/removeDuplicatesfromSortedArray.cpp
+++ C++/removeDuplicatesfromSortedArray.cpp
@@ -22,3 +22,20 @@ public:
     }
 };
 
+/*
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        int i = 0,j = 1;
+        while(j < nums.size())
+        {
+            if(nums[j] != nums[i])
+                nums[++i] = nums[j];
+            j++;
+        }
+        return i + 1;
+    }
+};
+*/
