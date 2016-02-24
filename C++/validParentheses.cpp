ddcffd20880ff5aae2044120ac0b716bc0376e59
Qu-Detao <qudet91@gmail.com>
2016-02-24 16:09:10 +0800

    Unify code indentation style

--- C++/validParentheses.cpp
+++ C++/validParentheses.cpp
@@ -6,20 +6,20 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
 
 class Solution {
 public:
-	bool isValid(string s) {
-		if (s.size() % 2)
-			return false;
-		stack<char> stk;
-		for (int i = 0; i < s.size(); ++i) {
-			if (s[i] == '(' || s[i] == '[' || s[i] == '{')
-				stk.push(s[i]);
-			else if (stk.size() == 0 || (s[i] == ')' && stk.top() != '(') ||
-				(s[i] == ']' && stk.top() != '[') ||
-				(s[i] == '}' && stk.top() != '{'))
-				return false;
-			else
-				stk.pop();
-		}
-		return stk.size() ? false : true;
-	}
+    bool isValid(string s) {
+        if (s.size() % 2)
+            return false;
+        stack<char> stk;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                stk.push(s[i]);
+            else if (stk.size() == 0 || (s[i] == ')' && stk.top() != '(') ||
+                (s[i] == ']' && stk.top() != '[') ||
+                (s[i] == '}' && stk.top() != '{'))
+                return false;
+            else
+                stk.pop();
+        }
+        return stk.size() ? false : true;
+    }
 };
