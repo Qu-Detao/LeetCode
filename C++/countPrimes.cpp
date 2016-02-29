ebd5c24253f2d2b824fe62c73c8a9c9c00a0dabb
Qu-Detao <qudet91@gmail.com>
2016-02-29 14:42:29 +0800

    Update solution for "Count Primes"

--- C++/countPrimes.cpp
+++ C++/countPrimes.cpp
@@ -7,17 +7,18 @@ class Solution {
 public:
     int countPrimes(int n){
         bool* isPrime = new bool[n];
-        for(int i = 2; i < n; ++i)
+        for(int i = 3; i < n; i += 2)
             isPrime[i] = true;
         
-        for(int i = 2; i * i < n; ++i){
+        for(int i = 3; i * i < n; i += 2){
             if(!isPrime[i]) continue;
-            for(int j = i * i; j < n; j += i)
+            for(int j = i * i; j < n; j += i + i)
                 isPrime[j] = false;
         }
         
         int count = 0;
-        for(int i = 2; i < n; ++i)
+        if(n > 2) count++;
+        for(int i = 3; i < n; i += 2)
             if (isPrime[i]) count++;
         
         delete[] isPrime;
