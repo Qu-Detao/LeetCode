2aaaaefcefda444f43bc186316e9bb04bd43ad9f
Qu-Detao <qudet91@gmail.com>
2016-03-03 15:04:41 +0800

    Add solution for "Search Insert Position"

---
+++ C++/searchInsertPosition.cpp
@@ -0,0 +1,25 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums.size() == 0 || nums[0] >= target)
+            return 0;
+        for(int i = 0; i < (int)nums.size() - 1; ++i){
+            if(nums[i] < target && nums[i + 1] >= target)
+                return i + 1;
+        }
+        return nums.size();
+    }
+};
