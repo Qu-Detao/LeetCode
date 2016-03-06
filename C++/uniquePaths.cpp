3802e6fc3c343ae50731625ebbe146f8f320f2d9
Qu-Detao <qudet91@gmail.com>
2016-03-06 10:05:08 +0800

    Add solution for "Unique Paths"

---
+++ C++/uniquePaths.cpp
@@ -0,0 +1,46 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach 
+the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long paths = 1;
+        for (int i = 1; i <= m - 1; ++i)
+            paths = paths * (n - 1 + i) / i;
+        return (int)paths;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > path(m, vector<int> (n, 1));
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+        return path[m - 1][n - 1];
+    }
+};
+
+// optimized version
+class Solution {
+    int uniquePaths(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        vector<int> cur(m, 1);
+        for (int j = 1; j < n; j++)
+            for (int i = 1; i < m; i++)
+                cur[i] += cur[i - 1]; 
+        return cur[m - 1];
+    }
+}; 
+*/
