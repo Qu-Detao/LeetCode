4e11c7477bc5839ecb967d7f637a6edc74d3808c
Qu-Detao <qudet91@gmail.com>
2016-03-31 17:10:10 +0800

    Update solution for "Remove Duplicates from Sorted Array II"

--- C++/removeDuplicatesFromSortedArrayII.cpp
+++ C++/removeDuplicatesFromSortedArrayII.cpp
@@ -12,20 +12,26 @@ being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty())
-            return 0;
-        int count = 0;
-        int curNum = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            int j = i;
-            while (i < nums.size() && nums[i] == curNum)
-                ++i;
-            if (i - j > 1)
-                count += i - j - 1;
-            curNum = i < nums.size() ? nums[i] : nums[i - 1];
-            nums[i - count - 1] = nums[i - 1];
-            nums[i - count] = curNum;
+        int j = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(j < 2 || nums[i] > nums[j - 2])
+                nums[j++] = nums[i];
         }
-        return nums.size() - count;
+        return j;
+    }
+};
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size(), count = 0;
+        for(int i = 2; i < n; ++i){
+            if(nums[i] == nums[i - 2 - count])
+                count++;
+            else
+                nums[i - count] = nums[i];
+        }
+        return n - count;
     }
 };
